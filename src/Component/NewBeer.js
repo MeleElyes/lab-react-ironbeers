a6e8559a92cc0820f3bfe1921815e3b66a79c02a
Elyes MELE <mele.elyes@gmail.com>
2023-01-09 16:38:42 +0100

    lab done

--- src/Component/NewBeer.js
+++ src/Component/NewBeer.js
@@ -12,11 +12,15 @@ const [send,setSend] = useState({name: "",
     attenuation_level: "",
     contributed_by: ""})
 
-const sendData = (event) => {
-    event.preventDefault();
+const sendData = (e) => {
+    e.preventDefault();
  axios.post('https://ih-beers-api2.herokuapp.com/beers/new',send) 
  .then(response => console.log(response)) 
  .catch(err => console.log(err))   
+}
+
+const changeObject= (e) => {
+    setSend((send) => ({...send,  [e.target.name]: e.target.value}))
 }
 console.log(send)
   return (
@@ -25,33 +29,33 @@ console.log(send)
       <form>
         <label>
           <span>Nom</span>
-          <input type="text" name="name" onChange={(event)=> setSend({...send, name : event.target.value})}/>
+          <input type="text" name="name" onChange={(e)=> changeObject(e)}/>
         </label>
         <label>
           <span>Tagline</span>
-          <input type="text" name="name" onChange={(event)=> setSend({...send, tagline : event.target.value})}/>
+          <input type="text" name="tagline" onChange={(e)=> changeObject(e)}/>
         </label>
         <label>
           <span>Description</span>
-          <input type="text" name="name" onChange={(event)=> setSend({...send, description : event.target.value})}/>
+          <input type="text" name="description" onChange={(e)=> changeObject(e)}/>
         </label>
         <label>
           <span>First Brewed</span>
-          <input type="text" name="name" onChange={(event)=> setSend({...send, first_brewed : event.target.value})}/>
+          <input type="text" name="first_brewed" onChange={(e)=> changeObject(e)}/>
         </label>
         <label>
           <span>Brewers Tips</span>
-          <input type="text" name="name" onChange={(event)=> setSend({...send, brewers_tips : event.target.value})}/>
+          <input type="text" name="brewers_tips" onChange={(e)=> changeObject(e)}/>
         </label>
         <label>
           <span>Attenuation Level</span>
-          <input type="number" name="name" onChange={(event)=> setSend({...send, attenuation_level : event.target.value})}/>
+          <input type="number" name="attenuation_level" onChange={(e)=> changeObject(e)}/>
         </label>
         <label>
           <span>Contributed by</span>
-          <input type="text" name="name" onChange={(event)=> setSend({...send, contributed_by : event.target.value})}/>
+          <input type="text" name="contributed_by" onChange={(e)=> changeObject(e)}/>
         </label>
-        <input className="AddNew" type="submit" value="Add New" onClick={(event)=> sendData(event) }/>
+        <input className="AddNew" type="submit" value="Add New" onClick={(e)=> sendData(e) }/>
       </form>
     </>
   );
